8883e21acfee217a071153db441060013406aa0d
lively-system-6174 <wkalada1@gmail.com>
2020-08-15 00:44:26 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,12 +36,16 @@ end
 def end_game
   # code #end_game here
   
-  puts 
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
   
 end
 
 def initial_round
   # code #initial_round here
+  
+  sum =  deal_card +
+  deal_card
+  
 end
 
 def hit?
